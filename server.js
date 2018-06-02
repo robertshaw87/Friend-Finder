e3e89b18d34e3d3072301ac05be59151194ed30d
Robert Shaw <robertshaw87@gmail.com>
2018-06-02 12:14:26 -0700

    fix file paths

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 3000;
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-require("./app/routes/api.js").attach(server);
-require("./app/routes/html.js").attach(server);
+require("./app/routing/apiRoutes.js").attach(server);
+require("./app/routing/htmlRoutes.js").attach(server);
 
 server.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);

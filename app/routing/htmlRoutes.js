e00b58f7bf9cf938b5c44df432c2acce043b8339
Robert Shaw <robertshaw87@gmail.com>
2018-06-02 11:51:29 -0700

    create initial htmls

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,11 @@ var path = require("path");
 var exports = module.exports = {};
 
 exports.attach = function (app) {
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+    app.get("/", function(request, response) {
+        response.sendFile(path.join(__dirname, "../public/home.html"));
     });
     
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    app.get("/survey", function(request, response) {
+        response.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 }

395dda77a37f4d350ba51ea307ccbc13237e931b
Robert Shaw <robertshaw87@gmail.com>
2018-06-04 22:11:18 -0700

    add initial styling to homepage

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+
 var server = express();
 var PORT = process.env.PORT || 3000;
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-server.use("/app/css", express.static(__dirname + "/public/css"));
-server.use("/app/script", express.static(__dirname + "/public/script"));
+server.use("/app", express.static(require("path").join(__dirname + "/app/public")));
 
 require("./app/routing/apiRoutes.js").attach(server);
 require("./app/routing/htmlRoutes.js").attach(server);
